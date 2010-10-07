c8df56638499960c6fadf02b5662c6246f76aa30
Ian Holmes <yam@minter-2.local>
2010-10-07 12:29:40 -0700

    quickbed.pl

--- quickbed.pl
+++ quickbed.pl
@@ -2,12 +2,22 @@
 
 # Usage: quickbed.pl [table names...]
 
+# files and directories
 my $hg19dir = ".";
-
-my @bedRequired = qw(chrom chromStart chromEnd);
-my @bedOptional = qw(name score strand);
-
 my $trackdb = "trackDb";
+
+# default columns to use for BED
+my @bedRequired = qw(chrom chromStart chromEnd);
+my @bedOptional = qw(name score strand thickStart thickEnd itemRgb blockCount blockSizes blockStarts);
+my @bedFieldNames = (@bedRequired, @bedOptional);
+
+# create customized handlers for particular tables (functions that map the table hashref to the BED hashref)
+my %tableHandler =
+    ("refGene" => {
+	"thickStart" => sub { shift->{"txStart"} },
+	"thickEnd" => sub { shift->{"txEnd"} },
+     },
+    );
 
 # Read all .sql files in directory - this was the old way of getting all the tables
 # local *DIR;
@@ -16,15 +26,16 @@ my $trackdb = "trackDb";
 # closedir DIR;
 
 # Find all the BED files in trackDb.txt.gz
-my @trackdbCols = name2column_list ($trackdb, qw(tableName shortLabel type));
+my %trackdbCols = name2column_map ($trackdb);
 my %trackdesc;
 my @sql;
 for_columns ($trackdb,
-	     \@trackdbCols,
+	     \%trackdbCols,
 	     sub {
-		 my ($tableName, $shortLabel, $type) = @_;
-		 if ($type =~ /^bed\b/) {
-		     $trackdesc{$tableName} = $shortLabel;
+		 my %row = @_;
+		 if ($row{'type'} =~ /^bed\b/) {
+		     my $tableName = $row{'tableName'};
+		     $trackdesc{$tableName} = $row{'shortLabel'};
 		     push @sql, $tableName;
 		 }
 	     });
@@ -39,9 +50,10 @@ for my $file (@ARGV) {
 # loop over all BED files (or all those named on command line)
 local $_;
 TABLE: for my $sql (@sql) {   # $sql = name of table (yes, bad choice of variable name)
+    # do some checks that we actually want to process this table and all the required files are there
     if (@ARGV && !grep ($_ eq $sql, @ARGV)) {
 #	warn "$sql not mentioned in argument list; skipping\n";
-	next;
+	next TABLE;
     }
 
     my ($sqlfile, $txtgzfile, $bedfile) = map ("$hg19dir/$sql.$_", qw(sql txt.gz bed));
@@ -61,23 +73,14 @@ TABLE: for my $sql (@sql) {   # $sql = name of table (yes, bad choice of variabl
 	next TABLE;
     }
 
-    my %name2col = name2column_map ($sql, @bedRequired, @bedOptional);
-    my @missing = grep(!defined($name2col{$_}), @bedRequired);
-    if (@missing) {
-	warn "$sql does not have required column(s): @missing\n";
-	next;
-    }
-
-    my @order = map ($name2col{$_}, @bedRequired, @bedOptional);
-    while (!defined($order[$#order])) { pop @order }
-
-    warn "Creating $sql.bed\n";
-    make_bed ($sql, \@order);
+    # create the BED file
+    warn "Creating $bedfile from $txtgzfile\n";
+    make_bed ($sql, $tableHandler{$sql});
 }
 
 # subroutine to crudely parse a .sql table description file and return a map from column names to column indices
 sub name2column_map {
-    my ($table, @colNames) = @_;
+    my ($table) = @_;
     my $sqlfile = "$table.sql";
 
     my @cols;
@@ -91,22 +94,12 @@ sub name2column_map {
     }
     close SQL;
 
-    return map ($cols[$_] =~ /^(@{[join("|",@colNames)]})$/
-		? ($1 => $_)
-		: (),
-		0..$#cols);
+    return map (($cols[$_] => $_), 0..$#cols);
 }
 
-# wrapper subroutine to return the values of name2column_map as an ordered list
-sub name2column_list {
-    my ($table, @colNames) = @_;
-    my %n2c = name2column_map ($table, @colNames);
-    return map ($n2c{$_}, @colNames);
-}
-
-# subroutine to crudely parse a .txt.gz table dump, select the indexed columns, and apply a given subroutine to each row so selected
+# subroutine to crudely parse a .txt.gz table dump, create a temporary hashref of column names to values for each row, and apply a given subroutine to this temporary hashref
 sub for_columns {
-    my ($table, $orderRef, $func) = @_;
+    my ($table, $name2colRef, $func) = @_;
 
     my $gzip = "gzip -cd $table.txt.gz";
     my $total = `$gzip | wc -l` + 0;
@@ -124,10 +117,8 @@ sub for_columns {
 	} else {
 	    $line .= $_;
 	    my @data = split /\t/, $line;
-	    &$func (map (defined() && defined($data[$_])
-			 ? $data[$_]
-			 : undef,
-			 defined($orderRef) ? @$orderRef : (0..$#data)));
+	    my %data = map (($_ => $data[$name2colRef->{$_}]), keys %$name2colRef);
+	    &$func (\%data);
 	    $line = "";
 	}
 	if (++$lines % 10000 == 0) { warn "(processed $lines/$total lines)\n" }
@@ -135,16 +126,38 @@ sub for_columns {
     close TXT;
 }
 
-# wrapper to print a BED file
+# wrapper to print a BED file, with an optional hashref mapping BED field names to handler functions that take the table row's colname=>value hashref as input, and return the corresponding BED field as output
 sub make_bed {
-    my ($table, $orderRef, $func) = @_;
-    $func = sub { @_ } unless defined $func;
+    my ($table, $handlerRef) = @_;
+
+    # get the column name->index map
+    my %name2col = name2column_map ($table);
+
+    # add default handlers
+    $handlerRef = {} unless defined $handlerRef;
+    for my $bedFieldName (@bedFieldNames) {
+	if (!defined($handlerRef->{$bedFieldName})) {
+	    if (defined ($name2col{$bedFieldName})) {
+		$handlerRef{$bedFieldName} = sub { shift->{$bedFieldName} };
+	    } else {
+		$handlerRef{$bedFieldName} = sub { undef };
+	    }
+	}
+    }
+
+    # create BED file
     my $bedfile = "$table.bed";
     local *BED;
     open BED, ">$bedfile" or die "$bedfile: $!";
     print BED "track name=$table description=\"$trackdesc{$table}\"\n";
     for_columns ($table,
-		 $orderRef,
-		 sub { print BED join (" ", map (defined() ? $_ : ".", &$func(@_))), "\n" });
+		 \%name2col,
+		 sub {
+		     my ($tableRowRef) = @_;
+		     my @bedFields = map (&{$handlerRef->{$_}} ($tableRowRef), @bedFieldNames);
+		     while (@bedFields && !defined($bedFields[$#bedFields])) { pop @bedFields }
+		     # could check that required fields are all defined here, I guess...
+		     print BED join (" ", map (defined() ? $_ : ".", @bedFields)), "\n";
+		 });
     close BED or die "$bedfile: $!";
 }
